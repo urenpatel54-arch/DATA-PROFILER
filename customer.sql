141a5773d87febf2f756cdb9fb5a8597ed8bedc6
urenpatel54-arch <urenpatel54@gmail.com>
2026-06-10 12:26:20 +0530

    Add files via upload

---
+++ customer.sql
@@ -0,0 +1,15 @@
+CREATE TABLE customers(
+CustomerID INT,
+Age INT,
+Gender VARCHAR(10),
+Income INT,
+Purchases INT,
+Churn VARCHAR(5)
+);
+
+INSERT INTO customers VALUES
+(3001,29,'Male',45000,6,'No'),
+(3002,41,'Female',70000,11,'No'),
+(3003,25,'Male',33000,2,'Yes'),
+(3004,50,'Female',85000,15,'No'),
+(3005,34,'Male',52000,8,'No');
